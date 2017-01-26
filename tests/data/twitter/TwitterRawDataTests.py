7cc630d6b210bcc9d862b4e294b57ba80f13877c
Chris Schultz <chris@Chriss-MacBook-Pro.local>
2017-01-26 15:31:23 -0600

    updated comments

--- tests/data/twitter/TwitterRawDataTests.py
+++ tests/data/twitter/TwitterRawDataTests.py
@@ -4,13 +4,13 @@ import unittest
 class TwitterRawDataTests(unittest.TestCase):
     def test_iget_json_data_entry(self):
         '''
-        Tests the iget_json_data_entry method to make sure it is actually importing raw twitter data json objects
+        Tests the iget_json_data_entry method to make sure the function is actually importing raw twitter data json objects
 
         Takes about 20 seconds to run
         '''
         twitter_json_raw_entries = [json_object for json_object in iget_json_data_entry()]
 
-        # test to make sure actually extracted something from json files
+        # test that json objects were extracted from json files
         # each json file contains about 840 entries so make sure got more than just one json file
         self.assertGreater(twitter_json_raw_entries, 1000)
 
